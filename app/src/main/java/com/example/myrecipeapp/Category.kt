fca09cc3db8e56671361043d51306d006e46da66
Prithvi <pc257773@gmail.com>
2025-05-06 21:29:22 +0530

    Hello

--- app/src/main/java/com/example/myrecipeapp/Category.kt
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -1,9 +1,10 @@
 package com.example.myrecipeapp
 
+@Parcelize
 data class Category(val idCategory:String,
     val strCategory:String,
     val strCategoryThumb:String,
-    val strCategoryDescription:String)
+    val strCategoryDescription:String):Parcelable
 
 
 data class CategoriesResponse(val categories:List<Category>)
